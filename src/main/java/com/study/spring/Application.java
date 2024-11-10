1067e9775b8d8dfdf6bc6d853b68d20787ce05d2
ksb9242 <ksb9242@gmail.com>
2024-11-10 21:05:42 +0900

    feat : 프로젝트 h2 Database 세팅 및 환경 세팅

--- src/main/java/com/study/spring/Application.java
+++ src/main/java/com/study/spring/Application.java
@@ -2,6 +2,7 @@ package com.study.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/com/study/spring/HelloController.java
@@ -0,0 +1,19 @@
+package com.study.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "ok";
+    }
+}
