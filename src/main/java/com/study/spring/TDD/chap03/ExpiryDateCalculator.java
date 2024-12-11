fb43f28644a7cef88964bf10a47d7a5864feb8de
ksb9242 <ksb9242@gmail.com>
2024-12-11 21:45:09 +0900

    [feat] : Chapter 3 매달 구독 서비스 테스트 케이스 작성

---
+++ src/main/java/com/study/spring/TDD/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,22 @@
+package com.study.spring.TDD.chap03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = 1;
+
+        if (payData.getFirstBillingDate() != null) {
+            // 후보 만료일 구하기
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+
+            // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                // 첫 납부일의 일자를 후보 만료일의 일자로 사용
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+        }
+
+        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+}
